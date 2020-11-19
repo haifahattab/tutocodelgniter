1adcd87a499695b9d54d84bd38a171c102c9a330
haifahattab <hattab_haifa@yahoo.com>
2020-11-19 17:05:55 +0100

    codeigniter

---
+++ bcit-ci-CodeIgniter-b73eb19/application/views/common/footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+      <hr>
+      <footer>
+        <p>&copy; EL ABED Haifa 2020</p>
+      </footer>
+      </div>
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
