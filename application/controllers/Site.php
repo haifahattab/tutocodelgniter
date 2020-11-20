10112192859d20f2b6f09fbf3b199f4007b7fbd3
haifahattab <hattab_haifa@yahoo.com>
2020-11-20 15:20:26 +0100

    send-mail

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -16,9 +16,28 @@ class Site extends CI_Controller {
         $data["title"] = "Contact";
 
         $this->load->view('common/header', $data);
-        if($this->form_validation->run()) {
-            // TODO: envoyer le mail
-            $this->load->view('site/contact_result', $data);
+		if($this->form_validation->run()){
+			$this->load->library('email');
+			$this->config->load('email', TRUE);
+			$this->email->initialize($this->config->item('email'));
+
+            $this->email->from($this->input->post('email'), $this->input->post('name'));
+            $this->email->to('hattab_haifa@yahoo.com');
+            $this->email->subject($this->input->post('title'));
+            $this->email->message($this->input->post('message'));
+				if($this->email->send()){
+					$data['result_class'] = "alert-success";
+					$data['result_message'] = "Merci de nous avoir envoyé ce mail. Nous y répondrons dans les meilleurs délais.";
+				} else {
+					$data['result_class'] = "alert-danger";
+					$data['result_message'] = "Votre message n'a pas pu être envoyé. Nous mettons tout en oeuvre pour résoudre le problème.";
+					// Ne faites jamais ceci dans le "vrai monde"
+					$data['result_message'] .= "<pre>\n";
+					$data['result_message'] .= $this->email->print_debugger();
+					$data['result_message'] .= "</pre>\n";
+					$this->email->clear();
+				}
+			$this->load->view('site/contact_result', $data);
         } else {
             $this->load->view('site/contact', $data);
         }
