b320231101a93739217e7709b4879c2bb543e27f
haifahattab <hattab_haifa@yahoo.com>
2020-11-20 10:54:36 +0100

    mise en forme

--- bcit-ci-CodeIgniter-b73eb19/application/views/common/header.php
+++ bcit-ci-CodeIgniter-b73eb19/application/views/common/header.php
@@ -4,14 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $title ?></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.theme.css">
+    <title><?= $title ?></title>    
+    <link rel="stylesheet" href="<?= base_url()."assets/css/bootstrap.min.css";?>">
+    <link rel="stylesheet" href="<?= base_url()."assets/css/theme.css";?>">
+    <script src="jquery-2.1.1.min.js"></script>
+    <script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -20,19 +18,12 @@
     <![ endif ] -->
   </head>
   <body>
-    <nav class="navbar navbar-inverse bg-dark mb-3">
+    <nav class="navbar navbar-inverse bg-dark mb-2">
       <div class="container-fluid">
-        <div class="navbar-header">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+        <div class="navbar-header row">
           <a class="navbar-brand" href="/index.html">EL ABED Haifa</a>
-        </div>
-        <div>
-          <ul class="nav navbar-nav">
-            <li><a href="/index.html">Accueil</a></li>
+          <ul class="nav navbar-brand">
+            <li><a href="<?= site_url('index'); ?>">Accueil</a></li>
           </ul>
         </div>
       </div>
